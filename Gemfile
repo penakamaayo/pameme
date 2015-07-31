efb19f91cd31497ac9e6c4a6413325a4e95657f8
james <penakamaayo@gmail.com>
2015-07-31 13:24:37 +0800

    setup gif search

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 ruby '2.2.2'
 
 gem 'sinatra', '~> 1.1.0'
+gem 'giphy', '~> 2.0.2'
--- pameme.rb
+++ pameme.rb
@@ -5,3 +5,36 @@ get '/' do
   'Pamemeeehhhh..'
 end
 
+post '/' do
+  content_type :json
+
+  if params_present? && meme
+    parse_url random_gif.original_image.url
+  else
+    parse_msg 'Walang ganun. sarree - :james:'
+  end
+end
+
+
+
+
+
+def params_present?
+  params.has_key?('text') && params.has_key?('trigger_word')
+end
+
+
+def parse_url url
+  {:text => url}.to_json
+end
+
+
+def meme
+  @pameme ||= Giphy.search(params[:text]).sample
+end
+
+
+def search_term
+  params[:text].gsub(params[:trigger_word], '').strip
+end
+
